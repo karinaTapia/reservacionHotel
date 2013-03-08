b979b53f7ea4177cd4d80695dee2bf7bb229b83f
Karina Tapia <mitzyrinami@hotmail.com>
2013-03-08 14:34:37 -0600

    res

--- js/API/eventos.js
+++ js/API/eventos.js
@@ -37,7 +37,7 @@ function reservar()
 {
 	alert("reserva");
 	//todos los elemetos ocn clse OPc que esten dentro d ela lista, al darles click  muestra un alert con el index del elemento
-	$('#nr1 ul[data-role=listview].opc').click(function(){
+	$('#nr1 ul[data-role=listview] .opc').click(function(){
 		alert("reserva2");
 			//alert($(this).index());
 			switch($(this).index())
